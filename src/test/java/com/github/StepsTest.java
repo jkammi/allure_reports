97c1819f7e69197c420855ffd83159ade8a7837c
Kamila Adigamova <uuueesi@gmail.com>
2023-04-07 22:13:30 +0200

    added tests with steps

---
+++ src/test/java/com/github/StepsTest.java
@@ -0,0 +1,55 @@
+package com.github;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+import static org.openqa.selenium.By.linkText;
+
+public class StepsTest {
+
+    private static final String REPOSITORY = "jkammi/aliexpress_automated_tests";
+    private static final String ISSUENAME = "TestIssue2";
+
+    @Test
+    public void testLambdaStep() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        step("Opening the mainpage", () -> {
+            open("https://github.com");
+        });
+        step("Looking for repository " + REPOSITORY, () -> {
+            $(".header-search-input").click();
+            $(".header-search-input").sendKeys(REPOSITORY);
+            $(".header-search-input").submit();
+        });
+        step("Clicking on the link of repository " + REPOSITORY, () -> {
+            $(linkText(REPOSITORY)).click();
+        });
+        step("Opening tab Issues", () -> {
+            $("#issues-tab").click();
+        });
+        step("Checking if the issue with this name exists: " + ISSUENAME, () -> {
+            $(withText(ISSUENAME)).should(Condition.exist);
+        });
+    }
+
+    @Test
+    public void testAnnotatedStep() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        WebSteps steps = new WebSteps();
+
+        steps.openMainPage();
+        steps.searchForRepository(REPOSITORY);
+        steps.clickOnRepositoryLink(REPOSITORY);
+        steps.openIssuesTab();
+        steps.shouldSeeIssueWithName(ISSUENAME);
+
+    }
+
+}
---
+++ src/test/java/com/github/WebSteps.java
@@ -0,0 +1,51 @@
+package com.github;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.WebDriverRunner;
+import io.qameta.allure.Attachment;
+import io.qameta.allure.Step;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+
+public class WebSteps {
+
+    public WebSteps() {
+    }
+
+    @Step("Opening the mainpage")
+    public void openMainPage() {
+        open("https://github.com");
+    }
+
+    @Step("Looking for repository {repo}")
+    public void searchForRepository(String repo) {
+        $(".header-search-input").click();
+        $(".header-search-input").sendKeys(repo);
+        $(".header-search-input").submit();
+    }
+
+    @Step("Clicking on the link of repository {repo}")
+    public void clickOnRepositoryLink(String repo) {
+        $(linkText(repo)).click();
+    }
+
+    @Step("Opening tab Issues")
+    public void openIssuesTab() {
+        $("#issues-tab").click();
+    }
+
+    @Step("Checking if the issue with this name exists: {issue}")
+    public void shouldSeeIssueWithName(String issue) {
+        $(withText(issue)).should(Condition.exist);
+    }
+
+    @Attachment(value = "Screenshot", type = "image/png", fileExtension = "png")
+    public byte[] takeScreenshot() {
+        return ((TakesScreenshot)WebDriverRunner.getWebDriver()).getScreenshotAs(OutputType.BYTES);
+    }
+}
